f9146cce2aa93454eef4659de22732502913bf56
SimantaRajSarma <simantarajsarma@gmail.com>
2024-12-05 03:59:47 +0530

    [bug]: fixed react typo and used vite prebuild .env (remove dotenv since vite handles env VARIALBES)

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,19 +3,17 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-
-
 const firebaseConfig = {
-  apiKey: import.meta.env.API_KEY,
-  authDomain: "chat-app-b1340.firebaseapp.com",
-  projectId: "chat-app-b1340",
-  storageBucket: "chat-app-b1340.firebasestorage.app",
-  messagingSenderId: "421082093688",
-  appId: "1:421082093688:web:8c6eb90c98b48f2e16758d",
-  measurementId: "G-4SJPD74F0V"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
 export const auth = getAuth();
 export const db = getFirestore();
